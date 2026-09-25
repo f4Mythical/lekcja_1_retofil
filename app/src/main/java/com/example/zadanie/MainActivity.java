cc6b4c47dad51bc363d263745ab37771922d57fa
f4Mythical <grynka.adam@gmail.com>
2026-09-25 09:56:42 +0200

    add elements in mainactivty

--- app/src/main/java/com/example/zadanie/MainActivity.java
+++ app/src/main/java/com/example/zadanie/MainActivity.java
@@ -22,3 +22,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+// https://my-json-server.typicode.com/f4Mythical/json
---
+++ app/src/main/java/com/example/zadanie/Pytanie.java
@@ -0,0 +1,4 @@
+package com.example.zadanie;
+
+public class Pytanie {
+}
